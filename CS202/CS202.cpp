1eb26a80eb54ba0106b5ec34ad46713c9050b3fe
linhlrx <64944040+linhlrx@users.noreply.github.com>
2021-01-08 23:42:55 +0700

    update random traffic light

--- CS202/CS202.cpp
+++ CS202/CS202.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-//#include <sfml\Graphics.hpp>
+#include <sfml\Graphics.hpp>
 #include "Controllers/MainMenu.h"
 
 #include "Modules/Vehicle/CarFactory.h"
